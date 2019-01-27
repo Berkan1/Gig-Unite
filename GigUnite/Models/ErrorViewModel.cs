73e798194fb366535b16dc81a470c95f1fe09d60
Berkan1 <berkan.c@hotmail.com>
2019-01-27 21:00:49 +0000

    Start of project

---
+++ GigUnite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GigUnite.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
