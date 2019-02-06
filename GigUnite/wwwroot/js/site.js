1a3610c1e6daf315d5b9bf026385f034bb73ccbb
Berkan1 <berkan.c@hotmail.com>
2019-02-06 18:33:36 +0000

    JS calculates age from dob

--- GigUnite/wwwroot/js/site.js
+++ GigUnite/wwwroot/js/site.js
@@ -3,8 +3,33 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
+
+    calculateAge(document.getElementById("age").innerHTML);
+
     $('#genres').multiselect({
         columns: 2,
         placeholder: 'Select genres'
     });
 });
+
+function calculateAge(dob) {
+    var dobValues = dob.split("/");
+
+    var d = new Date();
+    var dobYear = parseInt(dobValues[2]);
+    var dobMonth = parseInt(dobValues[1]);
+    var dobDay = parseInt(dobValues[0]);
+    var currentYear = d.getFullYear();
+    var currentMonth = d.getMonth() + 1;
+    var currentDay = d.getDate();
+
+    var age = currentYear - dobYear;
+    if (currentMonth < dobMonth) {
+        age = age - 1;
+    }
+    if (currentMonth == dobMonth && currentDay < dobDay) {
+        age = age - 1;
+    }
+
+    document.getElementById("age").innerHTML = age;
+}
